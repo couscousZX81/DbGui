9f01bad16ab8339e49b7a848a10390c34ec216e0
couscousZX81 <guynewbery@gmail.com>
2017-09-17 12:28:11 +0100

    Update DbGui.cpp

--- DbGui.cpp
+++ DbGui.cpp
@@ -4,16 +4,59 @@ namespace DbGui
 {
   struct context
   {
-    int keybits;
-    int lastitem;
+    int keyDown;
+    int lastItem;
     int currentX;
     int currentY;
     int labelWidth;
     int fieldWidth;
   } _context;
   
-  void menu::start()
+  void Menu::start()
   {
-    m_hotItem = 0;
+    m_hotItem.clear();
+  }
+  
+  void Menu::finish()
+  {
+    if (!(_context.KeyDown & INPUT_GO))
+      m_activeItem->clear();
+  }
+  
+  void Menu::process()
+  {
+    start();
+    run();
+    finish();
+  }
+  
+  void process()
+  {
+    _context.keyDown = 0;
+    if (inputNext)
+      _context.keyDown &= INPUT_NEXT;
+    if (inputPrev)
+      _context.keyDown &= INPUT_PREV;
+    if (inputGo)
+      _context.keyDown &= INPUT_GO;
+    if (inputBack)
+      _context.KeyDown &= INPUT_BACK;
+      
+    if (_menu)
+      _menu->process();
+  }
+  
+  void init(int x, int y, int labelWidth, int fieldWidth)
+  {
+    _context.currentX = x;
+    _context.currentY = y;
+    _context.labelWidth = labelWidth;
+    _context.fieldWidth = fieldWidth;
+  }
+  
+  void button(const char* label)
+  {
+    
+    currentY += lineSpacing;
   }
 }
