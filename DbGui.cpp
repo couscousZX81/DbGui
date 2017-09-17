9f263637eb48b9cf7bcef91389aa699f7b2a5df6
couscousZX81 <guynewbery@gmail.com>
2017-09-17 12:05:39 +0100

    Update DbGui.cpp

--- DbGui.cpp
+++ DbGui.cpp
@@ -2,5 +2,18 @@
 
 namespace DbGui
 {
+  struct itemId
+  {
+    int name;
+  }
   
+  struct context
+  {
+    int keybits;
+    int lastitem;
+    int currentX;
+    int currentY;
+    int labelWidth;
+    int fieldWidth;
+  } _context;
 }
