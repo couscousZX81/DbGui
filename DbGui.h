f32c78eada5245a3a0c2eca30df2eb6e2b265dfd
couscousZX81 <guynewbery@gmail.com>
2017-09-17 12:06:04 +0100

    Update DbGui.h

--- DbGui.h
+++ DbGui.h
@@ -2,5 +2,17 @@
 
 namespace DbGui
 {
-  
+  class Menu
+  {
+  public:
+    void Process();
+    
+  private:
+    void Start();
+    void Finish();
+    virtual void Update();
+    
+    itemId m_hotItem;
+    itemId m_activeItem;
+  }
 }
