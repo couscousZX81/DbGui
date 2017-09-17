6e174ff2606701f936da2bcb6fb36d11d4396d74
couscousZX81 <guynewbery@gmail.com>
2017-09-17 13:16:02 +0100

    Update DbGuiMenus.cpp

--- DbGuiMenus.cpp
+++ DbGuiMenus.cpp
@@ -4,19 +4,19 @@
 class DebugMenuInactive : public DbGui::Menu
 {
 public:
-  virtual void Run() override
+  virtual void DoMenu() override
   {
     if (L1+Select)
       DbGui::SetMenu(&_debugMenuMain)
   }
 } _debugMenuInactive;
 
-class DebugMenuMenu : public DbGui::Menu
+class DebugMenuMain : public DbGui::Menu
 {
 public:
-  virtual void Run() override
+  virtual void DoMenu() override
   {
-    DbGui::Init(100, 100, 400, 400)
+    DbGui::SetMenuLayout(100, 100, 400, 400)
     
     if (DbGui::Button(this, "Button1")
       //do Button1 stuff
