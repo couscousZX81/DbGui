b89f71dd24cdc146fe028f7941c357a6b62330d9
couscousZX81 <guynewbery@gmail.com>
2017-09-17 12:57:02 +0100

    Update DbGui.h

--- DbGui.h
+++ DbGui.h
@@ -1,34 +1,7 @@
 
 
 namespace DbGui
-{
-  static const int lineSpacing = 10;
-  
-  static const int INPUT_NEXT = 0x001;
-  static const int INPUT_PREV = 0x002;
-  static const int INPUT_GO   = 0x004;
-  static const int INPUT_BACK = 0x008;
-  
-  class ItemId
-  {
-  public:
-    ItemId() 
-      : m_bEmpty(0)
-    {
-    }
-    
-    virtual ~ItemId()
-    {
-    }
-    
-    void set();
-    void clear() {m_bEmpty = 0;}
-    
-  private:
-    int m_name;
-    bool m_bEmpty;
-  }
-  
+{ 
   class Menu
   {
   public:
@@ -40,7 +13,6 @@ namespace DbGui
     virtual void run();
     
     itemId m_hotItem;
-    itemId m_activeItem;
   }
   
   Menu* _menu;
