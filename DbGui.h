c3d2e40db6da9337b0135b59940c39c10db4b0ae
couscousZX81 <guynewbery@gmail.com>
2017-09-19 16:21:07 +0100

    Update DbGui.h

--- DbGui.h
+++ DbGui.h
@@ -1,27 +1,12 @@
 
 
 namespace DbGui
-{         
-  struct ItemId
-  {
-    ItemId();
-    
-    bool m_bEmpty;
-    int m_Name;
-    
-    ItemId& operator=(const ItemID& other);
-    inline bool operator==(const X& lhs, const X& rhs){ return Compare(lhs,rhs) == 0; }
-    inline bool operator!=(const X& lhs, const X& rhs){ return Compare(lhs,rhs) != 0; }
-    int Compare(const ItemId& lhs, const ItemId& rhs);
-
-    void Init();
-  };
-  
+{   
   struct Menu
   {
     Menu();
     
-    ItemId m_cursorItem;
+    int m_highlightedLine;
     
     void (*m_pFn)(void);
   };
@@ -30,15 +15,11 @@ namespace DbGui
   {
     Context();
     
-    Menu* m_pActiveMenu;
-    ItemID m_currentItem;
-    ItemID m_previousItem;
+    Menu* m_pMenu;
+    int m_line;
     int m_keyDown;
-    int m_atY;
     
     void Process();
-    void DoCursorInput();
-    void FinishedItem();
     
     bool Button(const char *label);
   };
